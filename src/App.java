cadf42d9f6f8ae49251feab66a63766aaedee615
Graziane Santos <grazianesantos035@gmail.com>
2025-03-06 15:50:35 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int n = 10;
+        n = 2*3;
+        System.out.println("Hello, World!"+n);
+    }
+    public static int soma(int n1, int n2) {
+        return n1+n2;        
+    }
+}
+
