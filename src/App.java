b815f8236773bc3fb5d20ac2a25afc9e459409fc
Graziane Santos <grazianesantos035@gmail.com>
2025-03-06 16:05:55 -0300

    meu primeiro projeto java

--- src/App.java
+++ src/App.java
@@ -3,7 +3,9 @@ public class App {
         int n = 10;
         n = 2*3;
         System.out.println("Hello, World!"+n);
-        System.out.println("RODOU!")
+        System.out.println("RODOU!");
+}
+
     public static int soma(int n1, int n2) {
         return n1+n2;        
     }
